b4a8d9cd91e4faff83691bdb4b7a4d55d3aee144
Yajaira <yajaira_vasquezramos@hotmail.com>
2026-05-14 13:28:09 -0500

    Frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:3000/api';
+
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3000/api',
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
